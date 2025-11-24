f4d33204754a550e0da25fc9fddd86ed975d3ac9
ChristofferEJH <sircejh@gmail.com>
2025-11-24 10:52:57 +0100

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -14,4 +14,6 @@ T_matrix[0,1:] = T_sub_diag  # Super diag har ikke noget i første kolonne
 T_matrix[1,:] = T_diag
 T_matrix[2,:-1] = T_super_diag  # Sub diag har ikke noget i sidste kolonne 
 
-print(T_matrix)
+mass_electron = 1 # Definerer enhedsløst
+delta_x_lattice = (x_lattice[-1] - x_lattice[0]) / len(x_lattice)
+T_factor = 1 / (2*mass_electron*delta_x_lattice**2)
