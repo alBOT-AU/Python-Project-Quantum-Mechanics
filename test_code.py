651a773cf55fd7ef392b9562e6c1fe64eeba6398
ChristofferEJH <sircejh@gmail.com>
2025-11-24 10:43:08 +0100

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,1 +1,15 @@
+# Gitter
 x_lattice = np.linspace(-15, 15, 200)
+
+# Konstruer T
+T_matrix = np.zeros((3, 200))  # 3 for diagonalerne, 200 for størrelsen af matricen
+
+T_sub_diag = np.zeros(1,200) + 1 
+T_super_diag = np.zeros(1,200) + 1  
+T_diag = np.zeros(1,200) -2  
+
+T_matrix[0,1:] = T_sub_diag  # Super diag har ikke noget i første kolonne 
+T_matrix[1,:] = T_diag
+T_matrix[2,:-1] = T_super_diag  # Sub diag har ikke noget i sidste kolonne 
+
+print(T_matrix)
