52d88f01d0742c9f563857d46188bb7338be6f5a
ChristofferEJH <sircejh@gmail.com>
2025-11-24 11:14:46 +0100

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.linalg
+-----------------------------------------
 
 # Gitter
 x_lattice = np.linspace(-15, 15, 200)
@@ -16,4 +18,25 @@ T_matrix[2,:-1] = T_super_diag  # Sub diag har ikke noget i sidste kolonne
 
 mass_electron = 1 # Definerer enhedsløst
 delta_x_lattice = (x_lattice[-1] - x_lattice[0]) / len(x_lattice)
-T_factor = 1 / (2*mass_electron*delta_x_lattice**2)
+T_factor = -1 / (2*mass_electron*delta_x_lattice**2)
+
+T = T_factor * T_matrix
+
+# Konstruer V
+def V_factor(x, omega):
+  omega_square = omega**2
+  x_square = x**2
+  return omega_square * x_square / 2
+
+V_diag = V_factor(x_lattice, 2)
+V =  np.zeros((3, 200))
+V[1,:] = V_diag
+
+# Konstruer H
+H = T + V
+
+---------------------------------------------------------------------------
+# Eigenvalues and Eigenvectors
+eigvals, eigvecs = scipy.linalg.eigh_tridiagonal(H[1,:], H[0, 1:])  # Skal kun have diagonal + superdiagonal 
+
+
