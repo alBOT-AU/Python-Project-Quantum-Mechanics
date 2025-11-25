c8ff07c534588a6533acb37e619d1b825d664216
alBOT-AU <au805061@uni.au.dk>
2025-11-25 13:45:34 +0100

    Update and rename egenværdier_og_egenvektorer_for_et_kvantesystem.py to main_code.py

--- main_code.py
+++ main_code.py
@@ -3,6 +3,7 @@ import scipy.linalg
 import matplotlib.pyplot as plt
 import scipy.integrate
 #-----------------------------------------
+### Egenværdier og egenvektorer for et kvantesystem
 
 # Gitter
 x_lattice = np.linspace(-15, 15, 200)
@@ -67,3 +68,29 @@ def plot_normalized_eigenfunction(eigenvectors: np.ndarray, x_lattice: np.ndarra
     for i, axes in enumerate(ax):
         normalizing_factor = (scipy.integrate.simpson(abs(eigenvectors[i])**2, x_lattice))
         axes.plot(x_lattice, abs(eigenvectors[i])**2*normalizing_factor)
+
+
+#---------------------------------------------------------------------------
+### Tidspropagation
+
+def crank_nicholson_matrix(matrix_length: int, delta_t: float, n: int = 0) -> np.ndarray:
+    """
+
+    """
+    identity_matrix = np.zeros((1, matrix_length)) + 1
+    second_term = (-1)**n * ((1j * delta_t) / 2) * H
+    diag = identity_matrix + second_term[1, :]
+    sub_diag = second_term[0, 1:]
+    sup_diag = second_term[2, :-1]
+    return sub_diag, diag
+
+psis = np.array([eigvecs[0]])
+
+
+for i in range(10):
+    right_vector = crank_nicholson_matrix(200, 0.01, 1) @ psis[-1]
+    new_psi = scipy.linalg.solve_banded((1, 1), crank_nicholson_matrix(200, 0.01), right_vector)
+    psis.append(new_psi)
+
+print(psis)
+    
