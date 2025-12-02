85d6f1c1db01ae86dc9c135be93601476cd81b81
kristianaarhustech-boop <kristianaarhustech@gmail.com>
2025-12-02 11:09:47 +0100

    Update time_propagation.py

--- time_propagation.py
+++ time_propagation.py
@@ -1,12 +1,32 @@
 import numpy as np
+import numpy.ma as ma
 import scipy.linalg
 import matplotlib.pyplot as plt
 import scipy.integrate
-#-----------------------------------------
-t_grid = np.linspace(0, 100, 100)
+import matplotlib.animation as anim
+from matplotlib import rc
+plt.rc("animation", html="jshtml")
 
-mass_electron = 1
-delta_x_lattice = (x_lattice[-1] - x_lattice[0]) / len(x_lattice)
+def banded_mv(A, x):
+    y = A[1,:] * x
+    y[:-1] += A[0,1:] * x[1:]
+    y[1:]  += A[2,:-1] * x[:-1]
+    return y
+
+def potential(x: np.ndarray, V_0: float = 1e14, standard_deviation: float = 10, x_0: float = 40) -> np.ndarray:
+    """
+    Calculates the potential in form of a slim Gauss for a wave to collide with.
+
+    paramenters:
+        x: the values of which the potential is calculated
+        V_0: the initial value of the potential
+        standard_deviation: how much the function deviates
+        x_0: displacement of the top point along the x-axis in the positive direction
+    """
+
+    if standard_deviation == 0:
+        raise Exception("The standard deviation cannot be 0")
+    return V_0 * np.exp(-1 * ((x - x_0)**2) / 4 * standard_deviation**2)
 
 def construct_T(matrix_length: int, delta_x: float, mass: float) -> np.ndarray:
     """
@@ -18,7 +38,7 @@ def construct_T(matrix_length: int, delta_x: float, mass: float) -> np.ndarray:
         delta_x: length between x-values
         mass: mass of the particle
     """
-    T_matrix = np.zeros((3, 3000))
+    T_matrix = np.zeros((3, matrix_length))
 
     T_sub_diag = np.zeros((1,matrix_length - 1)) + 1
     T_super_diag = np.zeros((1,matrix_length - 1)) + 1
@@ -33,34 +53,13 @@ def construct_T(matrix_length: int, delta_x: float, mass: float) -> np.ndarray:
     T = T_factor * T_matrix
     return T
 
-def banded_mv(A, x):
-    y = A[1,:] * x
-    y[:-1] += A[0,1:] * x[1:]
-    y[1:]  += A[2,:-1] * x[:-1]
-    return y
-
-def potential(x: np.ndarray, V_0: float = 2, standard_deviation: float = 10,
-              x_0: float = 0) -> np.ndarray:
-    """
-    Calculates the potential in form of a slim Gauss for a wave to collide with.
-
-    paramenters:
-        x: the values of which the potential is calculated
-        V_0: the initial value of the potential
-        standard_deviation: how much the function deviates
-        x_0: displacement of the top point along the x-axis in the positive direction
-    """
-    if standard_deviation == 0:
-        raise Exception("The standard deviation cannot be 0")
-    return V_0 * np.exp(-1 * ((x - x_0)**2) / 4 * standard_deviation**2)
-
 def crank_nicholson_matrix(matrix_length: int, x, delta_t, t: float, n: int = 0) -> np.ndarray:
     """
 
     """
     delta_x_lattice = x[1] - x[0]
     V = potential(x)
-    T = construct_T(3000, delta_x_lattice, mass_electron)
+    T = construct_T(len(x), delta_x_lattice, mass_electron)
     H = T + V
     second_term = (-1)**n * ((1j * delta_t) / 2) * H
     second_term[1,:] += 1
@@ -75,14 +74,14 @@ def construct_wave(position, variance, central_speed, center_position):
 
 spread = 5
 variance = spread ** 2
-center_speed = np.sqrt(8)
+center_speed = -1
 center_position = 0
-x_lattice = np.linspace(-100, 100, 3000)
+x_lattice = np.linspace(-150, 150, 3000)
 dt = 0.01
 wave_0 = construct_wave(x_lattice, variance, center_speed, center_position)
 
-fig, ax = plt.subplots(3, 1)
-ax[0].plot(abs(construct_wave(x_lattice, variance, center_speed, center_position)**2))
+#fig, ax = plt.subplots(3, 1)
+#ax[0].plot(abs(construct_wave(x_lattice, variance, center_speed, center_position)**2))
 
 psis = [wave_0]
 for i in range(10000):
@@ -90,5 +89,22 @@ for i in range(10000):
     left_matrix = crank_nicholson_matrix(3000, x_lattice, dt, 0)
     new_psi = scipy.linalg.solve_banded((1, 1), left_matrix, right_vector)
     psis.append(new_psi)
-ax[1].plot(x_lattice, abs(psis[-1])**2)
-ax[1].plot(x_lattice, potential(x_lattice)/(max(potential(x_lattice)*20)))
+#ax[1].plot(x_lattice, abs(psis[-1])**2)
+#ax[1].plot(x_lattice, potential(x_lattice)/(max(potential(x_lattice)*20)))
+
+
+
+def ani_func(index):
+    normalizing_factor = 1 / (scipy.integrate.simpson(abs(psis[index*100])**2, x_lattice))
+    plot_func = normalizing_factor * abs((psis[index*100]))**2
+    line.set_ydata(plot_func)
+    return line,
+
+fig1, ani_ax = plt.subplots()
+potential_plot_normalize = 1 / max(potential(x_lattice)) * max(abs(psis[0])**2) * 1.2
+potential_plot = potential(x_lattice) * potential_plot_normalize
+ani_ax.plot(x_lattice, potential_plot, color="C3")
+line, = ani_ax.plot(x_lattice, abs(psis[0])**2)
+
+ani = anim.FuncAnimation(fig1, ani_func, frames=100, blit=False)
+ani
