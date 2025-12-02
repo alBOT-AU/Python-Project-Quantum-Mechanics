34d1c9324d290ce756da5afd34f2db3d3bd59404
alBOT-AU <au805061@uni.au.dk>
2025-12-02 14:47:37 +0100

    Update warm_up.py

--- warm_up.py
+++ warm_up.py
@@ -3,75 +3,69 @@ import scipy.linalg
 import matplotlib.pyplot as plt
 import scipy.integrate
 
-points = 2000
-x_lattice = np.linspace(-15, 15, points)
-t_grid = np.linspace(0, 100, 100)
-mass_electron = 1
-delta_x_lattice = (x_lattice[-1] - x_lattice[0]) / len(x_lattice)
-omega = 2
+class Eigenvalues_Harmonic_Oscillator():
+    def __init__(self, matrix_length: int = 2000, boundaries: float = 15, angular_frequency: float = 2, mass: float = 1):
+        """
+        Simulates the harmonic oscillation of a particle
 
-def construct_T(matrix_length: int, delta_x: float, mass: float) -> np.ndarray:
-    """
-    Constructs the T matrix, which consists of a tri-diagonal matrix. All other points than the diagonal,
-    super- and sub-diagonal have the value 0, so only those diagonals are represented as a matrix.
+        parameters:
+            matrix_length: amount of points in th matrix
+            boundaries: the positive and negative boundaries along the x-axis
+            angular_frequency: angular frequency of the particle
+            mass: mass of the particle
+        """
+        self.matrix_length = matrix_length
+        self.angular_frequency = angular_frequency
+        self.mass = mass
+        self.x_lattice = np.linspace(-boundaries, boundaries, self.matrix_length)
+        self.delta_x = (self.x_lattice[-1] - self.x_lattice[0]) / self.matrix_length
 
-    parameters:
-        matrix_length: length of the matrix
-        delta_x: length between x-values
-        mass: mass of the particle
-    """
-    T_matrix = np.zeros((3, points))
+    def construct_T(self) -> np.ndarray:
+        """
+        Constructs the T matrix, which consists of a tri-diagonal matrix. All other points than the diagonal,
+        super- and sub-diagonal have the value 0, so only those diagonals are represented as a matrix.
+        """
+        T_matrix = np.zeros((3, self.matrix_length))
 
-    T_sub_diag = np.zeros((1,matrix_length - 1)) + 1
-    T_super_diag = np.zeros((1,matrix_length - 1)) + 1
-    T_diag = np.zeros((1,matrix_length)) -2
+        T_sub_diag = np.zeros((1, self.matrix_length - 1)) + 1
+        T_super_diag = np.zeros((1, self.matrix_length - 1)) + 1
+        T_diag = np.zeros((1, self.matrix_length)) -2
 
-    T_matrix[0,1:] = T_sub_diag
-    T_matrix[1,:] = T_diag
-    T_matrix[2,:-1] = T_super_diag
+        T_matrix[0,1:] = T_sub_diag
+        T_matrix[1,:] = T_diag
+        T_matrix[2,:-1] = T_super_diag
 
-    T_factor = 1 / (2*mass*delta_x**2)
+        T_factor = - 1 / (2* self.mass * self.delta_x**2)
 
-    T = T_factor * T_matrix
-    return T
+        T = T_factor * T_matrix
+        return T
 
-def construct_V(x: np.ndarray, omega: float):
-    """
-    Constructs the V matrix, which consists of a tri-diagonal matrix.
+    def construct_V(self):
+        """
+        Constructs the V matrix, which consists of a tri-diagonal matrix. All other points than the diagonal,
+        super- and sub-diagonal have the value 0, so only those diagonals are represented as a matrix.
+        """
+        omega_square = self.angular_frequency**2
+        x_square = self.x_lattice**2
+        V_diag = omega_square * x_square / 2
+        V =  np.zeros((3, self.matrix_length))
+        V[1,:] = V_diag
+        return V
 
-    parameters:
-        x: x-values to make the matrix over
-        omega: angular frequency of the particle
-    """
-    omega_square = omega**2
-    x_square = x**2
-    V_diag = omega_square * x_square / 2
-    V =  np.zeros((3, points))
-    V[1,:] = V_diag
-    return V
+    def get_normalized_eigenfunction(self, n: int)->None:
+        """
+        Calculates the normalized eigenfunction for the n'th eigenvalue
 
-def get_normalized_eigenfunction(x: np.ndarray, matrix_length: int, 
-                                 delta_x: float, mass: float, 
-                                 angular_frequency: float, n: int = 0)->None:
-    """
-    Plots the normalizes functions for the first 10 eigenvalues.
+        parameters:
+            n: eigenfunction number
+        """
+        H = Eigenvalues_Harmonic_Oscillator.construct_T(self) + Eigenvalues_Harmonic_Oscillator.construct_V(self)
 
-    parameters:
-        x: x-values to determine the bounds of the plot
-        matrix_length: length of the matrix
-        delta_x: length between x-values
-        mass: mass of the particle
-        angular_frequency: angular frequency of the particle
-        n: eigenfunction number
-    """
-    H = construct_T(matrix_length, delta_x, mass) + construct_V(x, angular_frequency)
+        eigvals, eigvecs = scipy.linalg.eigh_tridiagonal(H[1,:], H[0, 1:])
+        normalizing_factor = 1 / (scipy.integrate.simpson(abs(eigvecs[:, n])**2, self.x_lattice))
+        return abs(eigvecs[:, n])**2*normalizing_factor
 
-    eigvals, eigvecs = scipy.linalg.eigh_tridiagonal(H[1,:], H[0, 1:])
-    normalizing_factor = 1 / (scipy.integrate.simpson(abs(eigvecs[:, n])**2, x))
-    return abs(eigvecs[:, n])**2*normalizing_factor
-    
-    
 fig, ax = plt.subplots(10, 1)
-
+specific_case = Eigenvalues_Harmonic_Oscillator()
 for i in range(10):
-    ax[i].plot(x_lattice, get_normalized_eigenfunction(x_lattice, points, delta_x_lattice, mass_electron, omega, i))
+    ax[i].plot(specific_case.x_lattice, specific_case.get_normalized_eigenfunction(i))
