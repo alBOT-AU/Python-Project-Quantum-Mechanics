46dc49bf933055486cdc46aefd9e20a93aa11524
alBOT-AU <au805061@uni.au.dk>
2025-12-04 13:56:38 +0100

    Update time_propagation.py

--- time_propagation.py
+++ time_propagation.py
@@ -6,13 +6,14 @@ import scipy.integrate
 from collections.abc import Callable
 plt.rc("animation", html="jshtml")
 
-class Particle_Collision():
+class ParticleCollision:
     def __init__(
-            self, wave: type[Callable], barrier: Callable, matrix_length: int = 3000,
+            self, wave: Callable, barrier: Callable, matrix_length: int = 3000,
             matrix_bounds: float = 150, mass: float = 1, dt: float = 0.1
     ):
         """
-        Simulates a collision between a particle and a barrier, and shows quantum tunneling for certain energies of the particle and barrier.
+        Simulates a collision between a particle and a barrier, and shows quantum tunneling for 
+        certain energies of the particle and barrier.
 
         parameters:
             wave: wave function
@@ -59,7 +60,7 @@ class Particle_Collision():
             n: even values (zero included) gives a positive sign, uneven values gives a negative sign
         """
         V = self.barrier
-        T = Particle_Collision.double_deriv(self)
+        T = ParticleCollision.double_deriv(self)
         H = T
         H[1,:] += V
         second_term = (-1)**n * ((1j * self.dt) / 2) * H
@@ -69,7 +70,7 @@ class Particle_Collision():
     def animate_collision(self, frame_space: int = 10, animation_points: int = 1000):
         """
         Animates the collision between the particle and the barrier.
-        
+
         parameters:
             frame_space: chooses every n'th point to show in animation, where n is the value assigned
             animation_points: amount of points to animate
@@ -80,20 +81,24 @@ class Particle_Collision():
             y[1:]  += A[2,:-1] * x[:-1]
             return y
 
-        fig1, ani_ax = plt.subplots()
+        fig1, ani_ax = plt.subplots(label = "test")
+        ani_ax.grid()
+        ani_ax.set_xlabel("Position")
+        ani_ax.set_ylabel("Energy")
+        ani_ax.set_title("Normalized wave function and linear relation to barrier")
         psis = [self.wave]
 
         potential_plot_normalize = 1 / max(self.barrier) * max(abs(psis[0])**2) * 1.2
         potential_plot = self.barrier * potential_plot_normalize
         ani_ax.plot(self.x_lattice, potential_plot, color="C3")
-        line, = ani_ax.plot(self.x_lattice, abs(psis[0])**2)
+        line, = ani_ax.plot(self.x_lattice, abs(psis[0])**2, label="Normalized wave function")
 
         for i in range(animation_points):
             right_vector = banded_mv(
-                Particle_Collision.crank_nicholson_matrix(self, i), psis[-1]
+                ParticleCollision.crank_nicholson_matrix(self, i), psis[-1]
             )
             left_matrix = (
-                Particle_Collision.crank_nicholson_matrix(self, 0)
+                ParticleCollision.crank_nicholson_matrix(self, 0)
             )
             new_psi = scipy.linalg.solve_banded((1, 1), left_matrix, right_vector)
             psis.append(new_psi)
@@ -108,11 +113,13 @@ class Particle_Collision():
         ani = anim.FuncAnimation(fig1, ani_func, frames = number_frames, blit=False)
         return ani
 
-def gaussian_potential(x_lattice: np.ndarray, height: float = 10, standard_deviation: float = 1, start_position: float = 40) -> np.ndarray:
+def gaussian_potential(
+        x_lattice: np.ndarray, height: float = 10, standard_deviation: float = 1, start_position: float = 40
+) -> np.ndarray:
         """
         Calculates the potential in form of a slim Gauss for a wave to collide with.
 
-        paramenters:
+        parameters:
             x_lattice: the values of which the potential is calculated
             height: height of the potential
             standard_deviation: how much the function deviates
@@ -122,10 +129,12 @@ def gaussian_potential(x_lattice: np.ndarray, height: float = 10, standard_devia
             raise Exception("The standard deviation cannot be 0")
         return height * np.exp(-1 * ((x_lattice - start_position)**2) / (4 * standard_deviation**2))
 
-def gaussian_wave(x_lattice: np.ndarray, center_position: float = 0, center_velocity: float = 4, variance: float = 2):
+def gaussian_wave(
+        x_lattice: np.ndarray, center_position: float = 0, center_velocity: float = 4, variance: float = 2
+):
     """
     Creates a wave with the form of a gaussian function.
-    
+
     parameters:
         x_lattice: the values of which the wave is calculated
         center_position: position for the center of the wave
@@ -138,5 +147,5 @@ def gaussian_wave(x_lattice: np.ndarray, center_position: float = 0, center_velo
     wave = factor_1 * factor_2 * factor_3
     return wave
 
-specific_collision = Particle_Collision(gaussian_wave, gaussian_potential)
+specific_collision = ParticleCollision(gaussian_wave, gaussian_potential, dt = 0.1)
 specific_collision.animate_collision(10, 1000)
